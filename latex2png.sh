163e13e4ecf269373bd8a25ec98c6f3b83b57db4
Trung Trương <trungtvq.work@gmail.com>
2020-03-09 13:44:13 +0700

    archived

--- latex2png.sh
+++ latex2png.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pdflatex main.tex 
-cp main.pdf output
-pdftoppm -png -q -f 1 -singlefile main.pdf output/
+pdftoppm -png -q -f 1 -singlefile main.pdf main
 rm main.tex -f
 rm main.aux -f
 rm main.log -f
+rm main.pdf -f
 #rm main.pdf -f
